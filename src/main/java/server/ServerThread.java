689da887a33009d5f78ee0208dc48d34f1e30366
byebye <uaaabbjjkl@gmail.com>
2017-02-23 15:16:06 +0100

    Formatting

--- src/main/java/server/ServerThread.java
+++ src/main/java/server/ServerThread.java
@@ -1,102 +1,103 @@
 package server;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ServerThread extends Thread {
-    private Socket socket;
-    private ThreadedServer server;
+  private Socket socket;
+  private ThreadedServer server;
 
-    public ServerThread(Socket clientSocket, ThreadedServer server) {
-        this.socket = clientSocket;
-        this.server = server;
+  public ServerThread(Socket clientSocket, ThreadedServer server) {
+    this.socket = clientSocket;
+    this.server = server;
+  }
+
+  private void processGet(DataOutputStream out, String fileName) throws IOException {
+    String xml = server.getFile(fileName);
+
+    if (xml == null)
+      out.writeBytes("ERROR No such file");
+    else
+      out.writeBytes(xml);
+  }
+
+  private void processSend(DataOutputStream out, String fileName, BufferedReader brinp) throws IOException {
+
+    StringBuilder xmlBuilder = new StringBuilder();
+
+    String line = brinp.readLine();
+    while (line != null) {
+      xmlBuilder.append(line);
+      line = brinp.readLine();
     }
 
-    private void processGet(DataOutputStream out, String fileName) throws IOException {
-        String xml = server.getFile(fileName);
+    String xml = xmlBuilder.toString();
+    // TODO validate XmlFile
 
-        if(xml == null)
-            out.writeBytes("ERROR No such file");
-        else
-            out.writeBytes(xml);
+    server.updateXml(fileName, xml);
+    out.writeBytes("OK");
+  }
+
+  public void run() {
+    InputStream inp;
+    BufferedReader brinp;
+    DataOutputStream out;
+    try {
+      inp = socket.getInputStream();
+      brinp = new BufferedReader(new InputStreamReader(inp));
+      out = new DataOutputStream(socket.getOutputStream());
     }
-
-    private void processSend(DataOutputStream out, String fileName, BufferedReader brinp) throws IOException {
-
-        StringBuilder xmlBuilder = new StringBuilder();
-
+    catch (IOException e) {
+      e.printStackTrace();
+      return;
+    }
+    while (!interrupted()) {
+      try {
+        System.err.println("Waiting for message...");
         String line = brinp.readLine();
-        while(line != null)
-        {
-            xmlBuilder.append(line);
-            line = brinp.readLine();
+        if (line == null) {
+          throw new IllegalArgumentException("Empty message.");
         }
-
-        String xml = xmlBuilder.toString();
-        // TODO validate XmlFile
-
-
-        server.updateXml(fileName, xml);
-        out.writeBytes("OK");
+        else if (line.startsWith("LIST")) {
+          System.err.print("List files...");
+          out.writeBytes(server.getList());
+          out.flush();
+          System.err.println(" done");
+        }
+        else if (line.startsWith("SEND ")) {
+          String fileName = line.substring(5);
+          System.err.print("Receive a file '" + fileName + "' from client...");
+          processSend(out, fileName, brinp);
+          out.flush();
+          System.err.println(" done");
+        }
+        else if (line.startsWith("GET ")) {
+          String fileName = line.substring(4);
+          System.err.print("Send the file '" + fileName + "' to client...");
+          processGet(out, fileName);
+          out.flush();
+          System.err.println(" done");
+        }
+        else {
+          throw new IllegalArgumentException("Unknown operation: '" + line + "'");
+        }
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+        break;
+      }
     }
-
-    public void run() {
-        InputStream inp;
-        BufferedReader brinp;
-        DataOutputStream out;
-        try {
-            inp = socket.getInputStream();
-            brinp = new BufferedReader(new InputStreamReader(inp));
-            out = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        while (!interrupted()) {
-            try {
-                System.err.println("Waiting for message...");
-                String line = brinp.readLine();
-                if (line == null) {
-                    throw new IllegalArgumentException("Empty message.");
-                }
-                else if (line.startsWith("LIST")) {
-                    System.err.print("List files...");
-                    out.writeBytes(server.getList());
-                    out.flush();
-                    System.err.println(" done");
-                }
-                else if (line.startsWith("SEND ")) {
-                    String fileName = line.substring(5);
-                    System.err.print("Receive a file '" + fileName + "' from client...");
-                    processSend(out, fileName, brinp);
-                    out.flush();
-                    System.err.println(" done");
-                }
-                else if (line.startsWith("GET ")) {
-                    String fileName = line.substring(4);
-                    System.err.print("Send the file '" + fileName + "' to client...");
-                    processGet(out, fileName);
-                    out.flush();
-                    System.err.println(" done");
-                }
-                else {
-                    throw new IllegalArgumentException("Unknown operation: '" + line + "'");
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
-        }
-        if (socket != null && socket.isConnected())
-            try {
-                System.err.println("Disconnecting from client.");
-                socket.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-    }
-
-
+    if (socket != null && socket.isConnected())
+      try {
+        System.err.println("Disconnecting from client.");
+        socket.close();
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+  }
 }
--- src/main/java/server/ThreadedServer.java
+++ src/main/java/server/ThreadedServer.java
@@ -14,112 +14,108 @@ import static common.Settings.dateTimeFormatter;
 
 public class ThreadedServer {
 
-    public static final int PORT = 1978;
+  public static final int PORT = 1978;
 
-    public static class XmlFile
-    {
-        String fileName;
-        String xml;
-        String SHA3;
-        LocalDateTime modificationDate;
+  public static class XmlFile {
+    String fileName;
+    String xml;
+    String SHA3;
+    LocalDateTime modificationDate;
+  }
+
+  private List<XmlFile> storedXmls = new LinkedList<>();
+
+  public synchronized String getList() {
+    StringBuilder sb = new StringBuilder();
+    for (XmlFile xml : storedXmls)
+      sb.append(xml.fileName).append(" ")
+          .append(xml.modificationDate.format(dateTimeFormatter)).append(" ")
+          .append(xml.SHA3).append("\n");
+    sb.append('\n');
+    return sb.toString();
+  }
+
+  public synchronized void updateXml(String fileName, String xmlString) {
+    for (XmlFile xml : storedXmls)
+      if (xml.fileName.equals(fileName)) {
+        // Create backup version
+        XmlFile backupXml = new XmlFile();
+        backupXml.fileName = xml.fileName + xml.modificationDate.format(dateTimeFormatter);
+        backupXml.xml = xml.xml;
+        backupXml.SHA3 = xml.SHA3;
+        backupXml.modificationDate = xml.modificationDate;
+
+        storedXmls.add(backupXml);
+
+        // Update element
+        xml.xml = xmlString;
+        xml.modificationDate = LocalDateTime.now();
+        xml.SHA3 = calculateSha256(xml.xml);
+
+        return;
+      }
+
+    // XmlFile not found, create new one
+    XmlFile newXml = new XmlFile();
+    newXml.fileName = fileName;
+    newXml.xml = xmlString;
+    newXml.modificationDate = LocalDateTime.now();
+    newXml.SHA3 = calculateSha256(newXml.xml);
+    storedXmls.add(newXml);
+  }
+
+  public synchronized String getFile(String name) {
+    for (XmlFile xml : storedXmls) {
+      if (xml.fileName.equals(name))
+        return xml.xml;
     }
 
-    private List<XmlFile> storedXmls = new LinkedList<>();
+    return null;
+  }
 
-    public synchronized String getList()
-    {
-        StringBuilder sb = new StringBuilder();
-        for(XmlFile xml: storedXmls)
-            sb.append(xml.fileName).append(" ")
-                .append(xml.modificationDate.format(dateTimeFormatter)).append(" ")
-                .append(xml.SHA3).append("\n");
-        sb.append('\n');
-        return sb.toString();
+  // Returns HEX representation of SHA-256
+  public String calculateSha256(String text) {
+    MessageDigest md;
+
+    try {
+      md = MessageDigest.getInstance("SHA-256");
+    }
+    catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+      throw new RuntimeException("No SHA-256 algorithm");
     }
 
-    public synchronized void updateXml(String fileName, String xmlString)
-    {
-        for(XmlFile xml : storedXmls)
-            if(xml.fileName.equals(fileName))
-            {
-                // Create backup version
-                XmlFile backupXml = new XmlFile();
-                backupXml.fileName = xml.fileName + xml.modificationDate.format(dateTimeFormatter);
-                backupXml.xml = xml.xml;
-                backupXml.SHA3 = xml.SHA3;
-                backupXml.modificationDate = xml.modificationDate;
+    md.update(text.getBytes(StandardCharsets.UTF_8));
+    byte[] bytes = md.digest();
 
-                storedXmls.add(backupXml);
-
-                // Update element
-                xml.xml = xmlString;
-                xml.modificationDate = LocalDateTime.now();
-                xml.SHA3 = calculateSha256(xml.xml);
-
-                return;
-            }
-
-        // XmlFile not found, create new one
-        XmlFile newXml = new XmlFile();
-        newXml.fileName = fileName;
-        newXml.xml = xmlString;
-        newXml.modificationDate = LocalDateTime.now();
-        newXml.SHA3 = calculateSha256(newXml.xml);
-        storedXmls.add(newXml);
+    StringBuilder sb = new StringBuilder();
+    for (byte b : bytes) {
+      sb.append(String.format("%02X", b));
     }
 
-    public synchronized String getFile(String name)
-    {
-        for(XmlFile xml : storedXmls)
-        {
-            if(xml.fileName.equals(name))
-                return xml.xml;
-        }
+    return sb.toString();
+  }
 
-        return null;
+  public static void main(String args[]) {
+    ThreadedServer server = new ThreadedServer();
+    ServerSocket serverSocket;
+
+    try {
+      serverSocket = new ServerSocket(PORT);
     }
-
-    // Returns HEX representation of SHA-256
-    public String calculateSha256(String text)
-    {
-        MessageDigest md;
-
-        try {
-            md = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            throw new RuntimeException("No SHA-256 algorithm");
-        }
-
-        md.update(text.getBytes(StandardCharsets.UTF_8));
-        byte[] bytes = md.digest();
-
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            sb.append(String.format("%02X", b));
-        }
-
-        return sb.toString();
+    catch (IOException e) {
+      e.printStackTrace();
+      return;
     }
-
-    public static void main(String args[]) {
-        ThreadedServer server = new ThreadedServer();
-        ServerSocket serverSocket;
-
-        try {
-            serverSocket = new ServerSocket(PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        while (true) {
-            try {
-                Socket socket = serverSocket.accept();
-                System.err.println("New client connection.");
-                new ServerThread(socket, server).start();
-            } catch (IOException e) {
-                System.out.println("I/O error: " + e);
-            }
-        }
+    while (true) {
+      try {
+        Socket socket = serverSocket.accept();
+        System.err.println("New client connection.");
+        new ServerThread(socket, server).start();
+      }
+      catch (IOException e) {
+        System.out.println("I/O error: " + e);
+      }
     }
+  }
 }
